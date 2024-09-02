d31f64438292f273c73985df1dc28067edc4c86c
Mook <kamolrat_saeboon@cmu.ac.th>
2024-09-03 00:14:47 +0700

    Done it Girls

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,9 +34,9 @@ export default function RootLayout({
           <Navbar />
           {children}
           <Footer
-            year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            year="2024"
+            fullName="Kamolrat Saeboon"
+            studentId="660610737"
           />
         </div>
       </body>
--- src/app/search/[searchInput]/page.js
+++ src/app/search/[searchInput]/page.js
@@ -1,9 +1,11 @@
 "use client";
 
-import { MovieRow } from "@/components/MovieRow";
-import { movieDB } from "@/libs/movieDB";
+import  MovieRow  from "@components/MovieRow";
+import { movieDB } from "@lib/movieDB";
+import { useParams } from "next/navigation";
 
 export default function SearchResultPage() {
+  const params = useParams();
   //tip1 : before filtering movie, replace all "%20" with " " (space) in the input
   // const processedSearchInput = ...
 
@@ -17,14 +19,28 @@ export default function SearchResultPage() {
     you code here...
   );
   */
+  const processedSearchInput = params.searchInput.replaceAll("%20"," ");
+  const filterMovie = movieDB.filter((findMovie) => findMovie.title.toLocaleLowerCase().includes(processedSearchInput.toLocaleLowerCase()) );
 
   return (
     <div>
       <p className="fw-bold fs-4 text-center my-0">
-        Searching &quot; ... &quot;
+        Searching &quot; {processedSearchInput} &quot;
       </p>
-      <p className="fw-bold fs-4 text-center">Found ... result(s)</p>
+      <p className="fw-bold fs-4 text-center">Found {filterMovie.length} result(s)</p>
       {/* Use  "filteredMovies" variable to map-loop rendering MovieRow component */}
+      <div className="mx-auto vstack gap-2" style={{ width: "70%" }}>
+        {filterMovie.map((movie, i) => (
+          <MovieRow
+            key={movie.id}
+            id={movie.id}
+            title={movie.title}
+            detail={movie.detail}
+            rating={movie.rating}
+            number={i + 1}
+          />
+        ))}
+      </div>
     </div>
   );
 }
